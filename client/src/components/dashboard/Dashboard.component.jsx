3e15848e033cdf7ae4c51bd59c0c1a060f46ff42
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-07-17 02:05:51 +0530

    minor page refs

--- client/src/components/dashboard/Dashboard.component.jsx
+++ client/src/components/dashboard/Dashboard.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Nav from '../common/Nav/Nav';
 import Sidebar from '../dashboard/components/Sidebar';
 import CardPage from './components/CardPage';
+import FacultySidebar from '../common/Nav/FacultySidebar';
 
 const Dashboard = () => {
   return (
     <div className=' flex  gap-4'>
-        <Nav/>
+        <FacultySidebar/>
         <CardPage/>
     </div>
   )
